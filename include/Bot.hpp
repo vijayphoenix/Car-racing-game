f0b01d9e921483ad5dc6ed8821ca58afdf995374
vijayphoenix <cs17btech11040@iith.ac.in>
2019-02-08 19:34:45 +0530

    Fixed bot car related issues

--- include/Bot.hpp
+++ include/Bot.hpp
@@ -10,15 +10,15 @@ namespace cp
 	{
 
 	public:
-		Bot(GameDataRef _data, int car_num,float position);
+		Bot(GameDataRef _data, int car_num);
 		~Bot();
 
 		void update_car(float dt, std::vector<Line> &lines, float pos, float segL)
 		{
-			float speedRatio = 1;
-			float dx = 2 * dt * speedRatio;
-			playerX -= lines[position/segL].curve;
-			car_image_num = 5;
+			// float speedRatio = 1;
+			// float dx = 2 * dt * speedRatio;
+			// playerX -= lines[position/segL].curve;
+			// car_image_num = 5;
 		}
 		float centrifugal = 0.5;
 		float friction = -max_speed / 5;
